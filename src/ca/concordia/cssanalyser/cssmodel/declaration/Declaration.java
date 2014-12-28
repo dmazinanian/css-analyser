1980880608e2fd38f91d8e5220ddf63b09af4352
dmazinanian <dmazinanian@gmail.com>
2014-12-28 13:38:59 -0500

    Fixed the typo (isImportan??!!)

--- src/ca/concordia/cssanalyser/cssmodel/declaration/Declaration.java
+++ src/ca/concordia/cssanalyser/cssmodel/declaration/Declaration.java
@@ -93,7 +93,7 @@ public abstract class Declaration extends CSSModelObject implements Cloneable {
 	 * Sets the !important value
 	 * @param isImportant
 	 */
-	public void isImportan(boolean isImportant) {
+	public void isImportant(boolean isImportant) {
 		this.isImportant = isImportant;
 	}
 
