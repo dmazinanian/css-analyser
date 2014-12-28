9674e5c5406367e20b3820b375990612a060e90b
dmazinanian <dmazinanian@gmail.com>
2014-12-28 14:15:16 -0500

    Fixed the problem of removing already added declaration values

--- src/ca/concordia/cssanalyser/migration/topreprocessors/less/LessMixinMigrationOpportunity.java
+++ src/ca/concordia/cssanalyser/migration/topreprocessors/less/LessMixinMigrationOpportunity.java
@@ -1,9 +1,11 @@
 package ca.concordia.cssanalyser.migration.topreprocessors.less;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import ca.concordia.cssanalyser.cssmodel.declaration.Declaration;
 import ca.concordia.cssanalyser.cssmodel.declaration.value.DeclarationValue;
@@ -40,10 +42,10 @@ public class LessMixinMigrationOpportunity extends MixinMigrationOpportunity {
 			List<DeclarationValue> values = new ArrayList<>();
 			for (DeclarationValue v : declarationWithHighestNumberOfLayers.getDeclarationValues())
 				values.add(v);
-			List<DeclarationValue> checkedValues = new ArrayList<>(); // We don't use a set because there may be duplicates. See equals for DeclarationValue
+			Set<Integer> checkedValuesIndices = new HashSet<>(); 
 			for (int i = 0; i < values.size(); i++) {
 				DeclarationValue value = values.get(i);
-				if (checkedValues.contains(value))
+				if (checkedValuesIndices.contains(i))
 					continue;
 				boolean valueAdded = false;
 				if (value.getCorrespondingStyleProperty() != null) {
@@ -52,9 +54,9 @@ public class LessMixinMigrationOpportunity extends MixinMigrationOpportunity {
 						toReturn.append(mixinValue);
 						// Check all the values related to this style property, so we skip them in other runs
 						for (int j = 0; j < values.size(); j++) {
-							if (!checkedValues.contains(values.get(j)) &&
+							if (!checkedValuesIndices.contains(j) &&
 									value.getCorrespondingStylePropertyAndLayer().equals(values.get(j).getCorrespondingStylePropertyAndLayer())) {
-								checkedValues.add(values.get(j));
+								checkedValuesIndices.add(j);
 								/*
 								 * Try to remove the separator (comma) related to this property and layer
 								 * If the next value is a separator and the value after the separator has 
@@ -63,7 +65,7 @@ public class LessMixinMigrationOpportunity extends MixinMigrationOpportunity {
 								if (j <= values.size() - 3 && 
 										values.get(j + 1).getType() == ValueType.SEPARATOR &&
 										value.getCorrespondingStylePropertyAndLayer().equals(values.get(j + 2).getCorrespondingStylePropertyAndLayer())) {
-									checkedValues.add(values.get(j + 1));								
+									checkedValuesIndices.add(j + 1);								
 								}
 							}
 						}
@@ -104,6 +106,5 @@ public class LessMixinMigrationOpportunity extends MixinMigrationOpportunity {
 		}
 		mixinReferenceStringBuilder.append(");");
 		return mixinReferenceStringBuilder.toString();
-	}
-		
+	}	
 }
