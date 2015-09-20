eaf4119d2f5fc219236c3bcc0f009154575634aa
dmazinanian <dmazinanian@gmail.com>
2015-09-20 19:45:16 -0400

    Remove unnecessary println

--- src/ca/concordia/cssanalyser/migration/topreprocessors/PreprocessorMigrationOpportunitiesDetector.java
+++ src/ca/concordia/cssanalyser/migration/topreprocessors/PreprocessorMigrationOpportunitiesDetector.java
@@ -2,6 +2,7 @@ package ca.concordia.cssanalyser.migration.topreprocessors;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import ca.concordia.cssanalyser.analyser.duplication.DuplicationDetector;
 import ca.concordia.cssanalyser.analyser.duplication.items.Item;
@@ -14,43 +15,44 @@ import ca.concordia.cssanalyser.cssmodel.selectors.Selector;
 import ca.concordia.cssanalyser.migration.topreprocessors.mixin.MixinMigrationOpportunity;
 
 public abstract class PreprocessorMigrationOpportunitiesDetector<T> {
-	
+
 	private final StyleSheet styleSheet;
-	
+
 	protected abstract MixinMigrationOpportunity<T> getNewPreprocessorSpecificOpportunity(Iterable<Selector> forSelectors);
 
 	public PreprocessorMigrationOpportunitiesDetector(StyleSheet styleSheet) {
 		this.styleSheet = styleSheet;
 	}
-	
+
 	public StyleSheet getStyleSheet() {
 		return this.styleSheet;
 	}
 
 	public List<? extends MixinMigrationOpportunity<T>> findMixinOpportunities() {
-		
+
 		// Remove intra-selector dependencies!
 		StyleSheet styleSheetWithRemovedDependencies = this.styleSheet.getStyleSheetWithIntraSelectorDependenciesRemoved();
-		
+
 		// Apply FP-Growth on property duplications
 		DuplicationDetector duplicationDetector = new DuplicationDetector(styleSheetWithRemovedDependencies);
 		duplicationDetector.findPropertyDuplications();
-	
+
 		List<ItemSetList> itemSetLists = duplicationDetector.fpGrowth(2, false);
-			
+
 		List<MixinMigrationOpportunity<T>> mixinMigrationOpportunities = new ArrayList<>();
-			
+
 		/*
 		 * Each of the item sets is an opportunity.
 		 * Because each of them consist of a list of selectors having declarations with the same properties 
 		 */
+		System.out.println(itemSetLists);
 		for (ItemSetList itemSetList : itemSetLists) {
-			
+
 			for (ItemSet itemSet : itemSetList) {
-				
+
 				MixinMigrationOpportunity<T> opportunity = getNewPreprocessorSpecificOpportunity(itemSet.getSupport());
 				mixinMigrationOpportunities.add(opportunity);
-				
+
 				// Declarations in a item have the same property
 				for (Item item : itemSet) {
 					List<Declaration> declarationsToAdd = new ArrayList<>();
@@ -59,7 +61,7 @@ public abstract class PreprocessorMigrationOpportunitiesDetector<T> {
 					 * in this case, we add individual declarations instead 
 					 */
 					boolean allVirtual = true;
-					// This loop does two things! 
+					// This loop does two things!
 					for (Declaration d : item) {
 						if (d instanceof ShorthandDeclaration && !((ShorthandDeclaration)d).isVirtual()) {
 							allVirtual = false;
@@ -76,20 +78,20 @@ public abstract class PreprocessorMigrationOpportunitiesDetector<T> {
 							String referenceVirtualShorthandProperty = referenceIndividual.getProperty();
 							for (int i = 1; i < declarationsToAdd.size(); i++) {
 								ShorthandDeclaration virtualShorthand = (ShorthandDeclaration)declarationsToAdd.get(i);
-								Declaration individualForThisProperty = 
+								Declaration individualForThisProperty =
 										virtualShorthand.getIndividualDeclarationForProperty(referenceVirtualShorthandProperty);
 								individualDeclarationsToAdd.add(individualForThisProperty);
 							}
 							opportunity.addDeclarationsWithTheSameProperty(individualDeclarationsToAdd);
 						}
 					} else { // If not all the declarations are virtual shorthands
-						opportunity.addDeclarationsWithTheSameProperty(declarationsToAdd);	
+						opportunity.addDeclarationsWithTheSameProperty(declarationsToAdd);
 					}
 				}
 			}
 		}
-		
+
 		return mixinMigrationOpportunities;
-		
+
 	}
 }
