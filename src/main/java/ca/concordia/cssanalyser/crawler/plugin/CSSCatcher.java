7e6cefc659969313ebb14cfc91d5f3181c06569b
Davood Mazinanian <dmazinanian@gmail.com>
2015-11-27 15:23:35 -0500

    Several improvements in CSSCatcher plugin
    - Try to save the CSS file using its original name, not SHA1 hash
    - Using another approach for reading remote files, rather than channels.
    Channels are closed if the corresponding thread is interrupted, which
    can be a problem in many cases when using Crawljax.

--- src/main/java/ca/concordia/cssanalyser/crawler/plugin/CSSCatcher.java
+++ src/main/java/ca/concordia/cssanalyser/crawler/plugin/CSSCatcher.java
@@ -1,28 +1,26 @@
 package ca.concordia.cssanalyser.crawler.plugin;
 
+import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.HttpURLConnection;
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.channels.Channels;
-import java.nio.channels.ReadableByteChannel;
+import java.net.URLConnection;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.apache.commons.codec.digest.DigestUtils;
 import org.slf4j.Logger;
-
-import ca.concordia.cssanalyser.app.FileLogger;
-import ca.concordia.cssanalyser.io.IOHelper;
 
 import com.crawljax.browser.EmbeddedBrowser;
 import com.crawljax.core.CrawlerContext;
 import com.crawljax.core.plugin.GeneratesOutput;
 import com.crawljax.core.plugin.OnNewStatePlugin;
 import com.crawljax.core.state.StateVertex;
+
+import ca.concordia.cssanalyser.app.FileLogger;
+import ca.concordia.cssanalyser.io.IOHelper;
 
 /**
  * This plugin, written for Crawljax, is responsible for 
@@ -60,17 +58,15 @@ public class CSSCatcher implements OnNewStatePlugin, GeneratesOutput {
 			
 			if (hrefObj != null) {
 				String href = hrefObj.toString();
-				//if (!cssHrefs.contains(href)) {
-					cssHrefs.add(href);
-					fetchAndWriteFile(href, arg1.getName(), arg1.getUrl());
-				//}
+				cssHrefs.add(href);
+				fetchAndWriteFile(href, arg1.getName(), arg1.getUrl());
 			}
 		}
 
 	}
 
 	/**
-	 * Fethes the file, given by href, and saves it to the
+	 * Fetches the file, given by href, and saves it to the
 	 * folder initialized with setOutputFolder()
 	 * @param href
 	 * @param forWebSite
@@ -84,84 +80,98 @@ public class CSSCatcher implements OnNewStatePlugin, GeneratesOutput {
 		String folderPath =  getOutputFolder() + "/" + stateName;
 		
 		// Create the desired folder. One folder for each state
-
 		File outputFolder = new File(folderPath);
 		if (!outputFolder.exists() || !outputFolder.isDirectory())
 			outputFolder.mkdir();
 		
+		int questionMark = href.indexOf("?");
+		if (questionMark >= 0)
+			href = href.substring(0, questionMark);
+		
 		int lastSlashPosition = href.lastIndexOf('/');
 		
-		if (href.contains("https://s.yimg.com/zz/combo?kx/yucs/uh3/uh/css/928/uh-min.css&kx/yucs/uh3/uh3_top_bar/css/278/no_icons-min.css&kx/yucs/uh3/search/css/513/blue_border-min.css&kx/yucs/uh3/uh/css/928/uh_ssl-min.css&nq/s/a/o/global-fresh_54_4354.css&nq/s/a/o/buttons-fresh_54_4354.css&nq/s/a/o/lists-fresh_54_4354.css&nq/s/a/o/boxes_54_4354.css&nq/s/a/o/mail-fresh_54_4354.css&nq/s/a/o/mail-candygram-fresh_54_4354.css&nq/s/a/style_54_4354.css&nq/s/a/messagelist_54_4354.css&nq/s/a/conversations_54_4354.css&nq/s/a/basepane_54_4354.css&nq/s/a/pane_54_4354.css&nq/s/a/notifications_54_4354.css&nq/s/a/ads_54_4354.css&nq/s/a/pc-overrides_54_4354.css&nq/s/a/compose_54_4354.css&nq/s/a/lozenge_54_4354.css&nq/s/a/search_54_4354.css&nq/s/a/boss_54_4354.css&nq/s/a/navigation_54_4354.css&nq/s/a/nav-tabs_54_4354.css&nq/s/a/sponsored-themes_54_4354.css&nq/s/a/theme/purple_54_4354.css&nq/s/a/o/thread-inbox_54_4354.css&nq/s/a/o/infscroll_54_4354.css&nq/s/a/o/vertPrev_54_4354.css&nq/s/a/o/thread-inbox-header-fresh_54_4354.css&nq/s/a/o/mod-thread-fresh_54_4354.css&nq/s/a/o/tutorial_54_4354.css&nq/s/a/o/searchtooltip_54_4354.css&nq/s/a/o/convstooltip_54_4354.css&nq/s/a/o/themestooltip_54_4354.css&nq/s/a/o/buttonctx_54_4354.css"))
-			System.out.print("");
-		
 		// Get the name of file and append it to the desired folder
-		String cssFileName = DigestUtils.shaHex(href.substring(lastSlashPosition));//href.substring(lastSlashPosition).replaceAll("[<>\\/?*:\"|]", "_");
-		//if (cssFileName.length() > 128)
-		//	cssFileName = cssFileName.substring(0, 128);
+		String cssFileName = href.substring(lastSlashPosition + 1).replaceAll("[\\\\\\/:\\*\\?\\\"\\<\\>\\|]", "_");
+		if (cssFileName.length() > 128)
+			cssFileName = cssFileName.substring(0, 128);
 		
-		// If file name does not end with .css, add it
-		//if (!cssFileName.endsWith("css"))
+		//If file name does not end with .css, add it
+		if (!cssFileName.endsWith(".css"))
 			cssFileName = cssFileName + ".css";
 		
 		String cssFilePath = folderPath + "/" + cssFileName;
 		
-		//while ((new File(cssFilePath)).exists())
-		//	cssFilePath += "_.css";
-		//if (!(new File(cssFilePath)).exists()) {
+		while ((new File(cssFilePath)).exists())
+			cssFilePath += "_.css";
+
 			
-			FileOutputStream fos = null;
+		try {
 
-			try {
+			StringBuilder builder = new StringBuilder();
 
-				fos = new FileOutputStream(cssFilePath);
+			if (!href.startsWith("file://")) {
+				getRemoteFileContents(href, builder);
+			} else {
+				String localFile = IOHelper.readFileToString(href.replaceFirst("file://[/]?", ""));
+				builder.append(localFile);
+			}
+			
+			if (builder.length() > 0) {
 
-				// Lets add some information to the head of this css file
-				String headerText = String.format("/* \n" +
-						" * Created by CSSCatcher plugin for Crawljax\n" +
-						" * CSS file is for Crawljax DOM state %s\n" +
-						" * CSS file was contained in %s" +
-						" * Downloaded from %s\n" +
-						" */\n\n",  forWebSite, stateName, href);
+				final String EOL_CHAR = "\n";
 
-				byte[] headerBytes = headerText.getBytes();
+				// Lets add some information to the head of this CSS file
+				String headerText = String.format("/* " + EOL_CHAR +
+						" * Created by CSSCatcher plugin for Crawljax" + EOL_CHAR +
+						" * CSS file is for Crawljax DOM state %s" + EOL_CHAR +
+						" * CSS file was contained in %s" + EOL_CHAR +
+						" * Downloaded from %s" + EOL_CHAR +
+						" */" + EOL_CHAR + EOL_CHAR,  forWebSite, stateName, href);
 
-				fos.write(headerBytes);
-				
-				if (!href.startsWith("file://")) {
-					URL remoteCSSFile = new URL(href);
-					HttpURLConnection urlConnection = (HttpURLConnection) remoteCSSFile.openConnection();
-					urlConnection.setUseCaches(false);
-					urlConnection.setDoOutput(false);
-					urlConnection.setReadTimeout(10000);
-					ReadableByteChannel rbc = Channels.newChannel(urlConnection.getInputStream());
-					fos.getChannel().transferFrom(rbc, headerBytes.length, Long.MAX_VALUE);
-				} else {
-					String localFile = IOHelper.readFileToString(href.replaceFirst("file://[/]?", ""));
-					fos.write(localFile.getBytes());
-				}
-
-			} catch (MalformedURLException e) {
-
-				LOGGER.warn("Malformed url for file:" + href);
-
-			} catch (IOException e) {
-
-				LOGGER.warn("IOException for file:" + href);
-				e.printStackTrace();
-
-			} finally {
-
-				try {
-					fos.close();
-					LOGGER.info("Saved file " + cssFilePath);
-				} catch (Exception e) {
-					// Swallow 
-				}
+				IOHelper.writeStringToFile(headerText + builder.toString().replace("\r\n", EOL_CHAR), cssFilePath);
 
 			}
-		}
-	//}
 
+		} catch (MalformedURLException e) {
+
+			LOGGER.warn("Malformed url for file:" + href);
+
+		} catch (IOException e) {
+
+			LOGGER.warn("IOException for file:" + href);
+			e.printStackTrace();
+
+		}
+	}
+	
+	private void getRemoteFileContents(String href, StringBuilder builder) {
+		try {
+			URLConnection urlConnection = (new URL(href)).openConnection();
+			int contentLength = urlConnection.getContentLength();
+			if (!"text/css".equals(urlConnection.getContentType()) || contentLength == -1) {
+				LOGGER.warn("{} is not a CSS file, skipping", href);
+			} else {
+				InputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
+				byte[] data = new byte[contentLength];
+				int bytesRead = 0;
+				int offset = 0;
+				while (offset < contentLength) {
+					bytesRead = inputStream.read(data, offset, data.length - offset);
+					if (bytesRead == -1)
+						break;
+					offset += bytesRead;
+				}
+				inputStream.close();
+
+				if (offset != contentLength) {
+					throw new IOException("Only read " + offset + " bytes; Expected " + contentLength + " bytes");
+				}
+				builder.append(new String(data));	
+			}
+		} catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
 	/**
 	 * Returns a collection of all CSS hrefs,
 	 * downloaded during crawling session.
