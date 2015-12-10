31aec625a1ba12fbd660896a74ccf8fe20e3abe6
Davood Mazinanian <dmazinanian@gmail.com>
2015-12-10 18:47:22 -0500

    Did I forget to commit ModifiedFileSource?

---
+++ src/main/java/ca/concordia/cssanalyser/parser/less/ModifiedFileSource.java
@@ -0,0 +1,38 @@
+package ca.concordia.cssanalyser.parser.less;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import org.apache.commons.io.IOUtils;
+
+import com.github.sommeri.less4j.LessSource.FileSource;
+
+public class ModifiedFileSource extends FileSource {
+
+	public ModifiedFileSource(File inputFile) {
+		super(inputFile);
+	}
+
+	@Override
+	public String getContent() throws FileNotFound, CannotReadFile {
+		try {
+			Reader input;
+			input = new FileReader(getInputFile());
+			try {
+				String content = IOUtils.toString(input); // .replace("\r\n", "\n");
+				setLastModified(getInputFile().lastModified());
+				return content;
+			} finally {
+				input.close();
+			}
+		} catch (FileNotFoundException ex) {
+			throw new FileNotFound();
+		} catch (IOException ex) {
+			throw new CannotReadFile();
+		}
+	}
+
+}
