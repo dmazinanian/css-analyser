21e42ecb1d50c374b5187625ebcb400bcc690c23
Davood Mazinanian <dmazinanian@gmail.com>
2016-04-27 20:56:45 -0400

    Distinguish pseudo class and element in hashCode()

--- src/main/java/ca/concordia/cssanalyser/cssmodel/selectors/PseudoClass.java
+++ src/main/java/ca/concordia/cssanalyser/cssmodel/selectors/PseudoClass.java
@@ -55,7 +55,7 @@ public class PseudoClass {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((name == null) ? 0 : name.hashCode());
-		result = prime * result + ((value == null) ? 0 : value.hashCode());
+		result = prime * result + ((value == null) ? 0 : (":" + value).hashCode());
 		return result;
 	}
 
--- src/main/java/ca/concordia/cssanalyser/cssmodel/selectors/PseudoElement.java
+++ src/main/java/ca/concordia/cssanalyser/cssmodel/selectors/PseudoElement.java
@@ -24,7 +24,7 @@ public class PseudoElement {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((name == null) ? 0 : ("::" + name).hashCode());
 		return result;
 	}
 
