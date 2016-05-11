71111c8aed8d80926b0435174e9b90dc3d65e909
Davood Mazinanian <dmazinanian@gmail.com>
2016-05-11 19:16:11 -0400

    Changed CSVColumns to accept different separators

--- src/main/java/ca/concordia/cssanalyser/io/CSVColumns.java
+++ src/main/java/ca/concordia/cssanalyser/io/CSVColumns.java
@@ -1,25 +1,39 @@
 package ca.concordia.cssanalyser.io;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVColumns {
 	
-	private static String SEPARATOR = "|";
+	private String separator = "|";
 	
-	private final String[] columns;
+	private final List<String> columns;
 	
 	public CSVColumns(String... columns) {
-		this.columns = columns;
+		this.columns = new ArrayList<>();
+		for (String column : columns) {
+			this.columns.add(column);
+		}
 	}
 	
 	public void setSeparator(String separator) {
-		SEPARATOR = separator;
+		this.separator = separator;
+	}
+	
+	public String getSeparator() {
+		return this.separator;
+	}
+	
+	public void addColumn(String columnName) {
+		this.columns.add(columnName);
 	}
 
 	public String getHeader(boolean addLineSeparator) {
 		StringBuilder toReturn = new StringBuilder();
-		for (int i = 0; i < columns.length; i++) {
-			toReturn.append(columns[i]);
-			if (i < columns.length - 1) {
-				toReturn.append(SEPARATOR);
+		for (int i = 0; i < columns.size(); i++) {
+			toReturn.append(columns.get(i));
+			if (i < columns.size() - 1) {
+				toReturn.append(this.separator);
 			} else { 
 				if (addLineSeparator) {
 					toReturn.append(System.lineSeparator());
@@ -31,10 +45,10 @@ public class CSVColumns {
 	
 	public String getRowFormat(boolean addLineSeparator) {
 		StringBuilder toReturn = new StringBuilder();
-		for (int i = 0; i < columns.length; i++) {
+		for (int i = 0; i < columns.size(); i++) {
 			toReturn.append("%s");
-			if (i < columns.length - 1) {
-				toReturn.append(SEPARATOR);
+			if (i < columns.size() - 1) {
+				toReturn.append(separator);
 			} else { 
 				if (addLineSeparator) {
 					toReturn.append(System.lineSeparator());
